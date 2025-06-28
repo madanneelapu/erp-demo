71bf461ad67000dd40e8bbb16f5b1a70cc75ad28
Madan Neelapu <madanneelapu@gmail.com>
2025-06-28 14:52:31 +0530

    logger

--- src/main/java/io/madan/erpdemo/datetimeexample/controller/DateExampleController.java
+++ src/main/java/io/madan/erpdemo/datetimeexample/controller/DateExampleController.java
@@ -2,6 +2,8 @@ package io.madan.erpdemo.datetimeexample.controller;
 
 import io.madan.erpdemo.datetimeexample.dto.DateExampleDto;
 import io.madan.erpdemo.datetimeexample.ibo.DateExampleIBO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/user-events")
 public class DateExampleController {
+
+    private static final Logger logger = LogManager.getLogger(DateExampleController.class);
+
+
     private final DateExampleIBO ibo;
 
     public DateExampleController(DateExampleIBO ibo) {
@@ -20,6 +26,8 @@ public class DateExampleController {
     @PostMapping
     public ResponseEntity<?> saveDate(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader,
                                       @RequestBody DateExampleDto reqDto) {
+        logger.info(reqDto);
+        logger.info(authHeader);
         ibo.saveEventDate(reqDto, authHeader);
         return ResponseEntity.ok("Saved successfully");
     }
